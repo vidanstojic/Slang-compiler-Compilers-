a97faa698aa179982c4d3b0a66f09eecd6f6faf1
markostojicic03 <mstojicic12923rn@raf.rs>
2024-12-26 18:01:30 +0100

    Instructions klasa i zapocet CodeGenerator

---
+++ tim_mstojicic12923_vstojic8223/src/main/java/org/raf/slang/codegen/CodeGenerator.java
@@ -0,0 +1,11 @@
+package org.raf.slang.codegen;
+
+
+/*
+* Ova klasa ce kod sluziti za generisanje medjukoda, ona ce biti nalik javinom bytecode-u.
+* */
+
+public class CodeGenerator {
+
+
+}
---
+++ tim_mstojicic12923_vstojic8223/src/main/java/org/raf/slang/codegen/Instructions.java
@@ -0,0 +1,89 @@
+package org.raf.slang.codegen;
+
+/*
+* Posto cemo na steku cuvati zapravo instrukcije koje je potrebno da izvrsimo, moramo imati klasu
+* za instrukcije(Instructions/cesto se skraceno pise i insn).
+* */
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public final class Instructions {
+    // unutar code cemo bukvalno imati svaku komandu koja je moguca u nasem jeziku
+    public enum Code{
+        POP,
+        PUSH_TRUE,// na stek stavljamo vrednost true
+        PUSH_FALSE,// na stek stavljamo vrednost false
+        PUSHI (1),// dodajemo neku brojcanu vrednost na stek
+        PUSH_FUNC (1), // dodajemo pokazivac neke funkcije na stek
+        RET,// signalizira kraj izvrsavanja neke funkcije
+        CALL (1), // ako imamo npr f(a,b,c) onda ceo reci CALL(3) jer pozivamo funkciju sa 3 parametra
+        SET_LOCAL (1),// ako imamo npr. x=5 onda treba da setujemo prom x vrednost
+        GET_LOCAL(1), // uzimanje neke promenljive sa steka, ako imamo npr print(x);
+       // Sledece stavke su za nizove
+        SET_INDEX,
+        GET_INDEX,
+        LENGTH,
+        PUSH,
+        COLLECT(1),
+        // ---------- kraj stavki za nizove
+        // posto u nasem jeziku imamo kontrolu toka programa, potrebno je da imamo ove jump-ove
+        // na primer ukoliko je tacan uslov if-a mi zelimo da skocimo na odredjeni tok programa,
+        // dok ukoliko uslov if-a nije tacan onda zelimo da skocimo do else
+        JUMP(1),
+        JUMP_TRUE(1),
+        JUMP_FALSE(1),
+        // unarni operatori
+        NEGATE,
+        // binarne operacije(operacije sa 2 operanda)
+        BIT_AND,
+        BIT_OR,
+        BIT_GT, // >
+        BIT_LT, // <
+        BIT_GTE, // >=
+        BIT_LTE,// <=
+        BIT_ET, // ==
+        BIT_PLUS,
+        BIT_MINUS,
+        BIT_MUL,
+        BIT_DIV,
+        BIT_CR,// ^
+
+        EXIT,
+        PUTC,
+        ;
+        public final int argCount;
+        Code()
+        {this(0);}
+        Code(int argCount)
+        {this.argCount = argCount;}
+    }
+    private Code opcode;
+    public Code opcode(){return opcode;}
+
+    public Instructions(Code opcode){
+        assert opcode.argCount == 0;
+        this.opcode = opcode;
+    }
+
+    public Instructions(Code opcode, long arg1){
+        assert opcode.argCount == 1;
+        this.opcode = opcode;
+        this.arg1 = arg1;
+    }
+    private long arg1 = -1;
+    public void setArg1(long arg1){
+        assert opcode.argCount >= 1;
+        this.arg1 = arg1;
+    }
+    public long getArg1(){return arg1;}
+    @Override
+    public String toString(){
+        var s = new StringBuilder();
+        s.append(opcode);
+        if(opcode.argCount >= 1){s.append(" ").append(arg1);}
+        return s.toString();
+    }
+    // dodati ovde printer(pretty print)
+
+}
